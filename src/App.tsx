495c5793442211f31e7094229d81309b5adb7539
Matt Orchard <morch028@uottawa.ca>
2020-12-08 22:03:18 -0500

    Initial project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BaseGraph from "./components/BaseGraph";
+import { paste } from "./helpers/clipboardHelpers";
+import GraphErrorBoundary from "./components/GraphErrorBoundary";
+import useLocalStorageBackedState from "./hooks/useLocalStorageBackedState";
 
-function App() {
+const sampleGraph = {
+  nodes: [{ id: "A" }, { id: "B" }, { id: "C" }, { id: "D" }],
+  links: [
+    { source: "A", target: "B" },
+    { source: "A", target: "C" },
+    { source: "B", target: "D" }
+  ]
+};
+
+const App = () => {
+  const [graphData, setGraphData] = useLocalStorageBackedState(
+    sampleGraph,
+    "graph-storage-v0"
+  );
+  const pasteGraph = async () => {
+    try {
+      const rawData = await paste();
+      const graphData = JSON.parse(rawData);
+      if (typeof graphData !== "object") {
+        throw new Error(
+          `Graph data must be an object, with properties "nodes" and "links"`
+        );
+      }
+      if (!Array.isArray(graphData.nodes)) {
+        throw new Error(`"data.nodes" must be an array`);
+      }
+      if (!Array.isArray(graphData.links)) {
+        throw new Error(`"data.links" must be an array`);
+      }
+      setGraphData(graphData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GraphErrorBoundary>
+        <BaseGraph data={graphData} />
+      </GraphErrorBoundary>
+      <aside className="corner-form">
+        <button type="button" onClick={pasteGraph}>
+          Paste Data
+        </button>
+      </aside>
     </div>
   );
-}
+};
 
 export default App;
