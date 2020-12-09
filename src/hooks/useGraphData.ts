1c1e0ddc2568c2117323351d34490901e23d68d0
Matt Orchard <morch028@uottawa.ca>
2020-12-08 22:55:07 -0500

    Switches serialization strategy from JSON to compact sequence

--- src/hooks/useGraphData.ts
+++ src/hooks/useGraphData.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { GraphData, GraphLink, GraphNode } from "react-d3-graph";
+type GD = GraphData<GraphNode, GraphLink>;
 
 const sampleGraph = {
   nodes: [{ id: "A" }, { id: "B" }, { id: "C" }, { id: "D" }],
@@ -10,22 +11,56 @@ const sampleGraph = {
   ]
 };
 
+const stringifyGraphLinks = (links: GraphLink[]) =>
+  links.map(({ source, target }) => [source, target]).join(",");
+
+const parseGraphLinks = (linksText: string) => {
+  const links: GraphLink[] = [];
+  linksText.split(",").forEach((linkText, index, textArray) => {
+    if (index % 2 !== 0) {
+      const prevLinkText = textArray[index - 1];
+      links.push({
+        source: prevLinkText,
+        target: linkText
+      });
+    }
+  });
+  return links;
+};
+
+const stringifyGraphNodes = (nodes: GraphNode[]) =>
+  nodes.map(node => node.id).join(",");
+
+const parseGraphNodes = (nodesText: string) =>
+  nodesText.split(",").map(id => ({ id }));
+
+const graphDataToSearchParams = (graphData: GD) => {
+  const params = new URLSearchParams();
+  params.set("nodes", stringifyGraphNodes(graphData.nodes));
+  params.set("links", stringifyGraphLinks(graphData.links));
+  return params;
+};
+
 const getInitialGraphData = () => {
-  try {
-    const searchParams = new URLSearchParams(window.location.search);
-    const rawData = searchParams.get("data");
-    if (rawData) return JSON.parse(rawData);
-  } catch {}
+  const searchParams = new URLSearchParams(window.location.search);
+  const nodesRaw = searchParams.get("nodes");
+  const linksRaw = searchParams.get("links");
+  if (nodesRaw && linksRaw) {
+    return {
+      nodes: parseGraphNodes(nodesRaw),
+      links: parseGraphLinks(linksRaw)
+    };
+  }
+
   return sampleGraph;
 };
 
-type GD = GraphData<GraphNode, GraphLink>;
 const useGraphData = (): [GD, (graphData: GD) => void] => {
   const [graphData, setGraphData] = useState(getInitialGraphData);
 
   useEffect(() => {
     const url = new URL(
-      `?data=${encodeURIComponent(JSON.stringify(graphData))}`,
+      `?${graphDataToSearchParams(graphData)}`,
       window.location.href
     );
 
